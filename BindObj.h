02d0705cc3a45334d6af2561fbedff7224a9ba86
OWenT <owt5008137@live.com>
2013-09-29 11:42:07 +0800

    1.4.0 更新
    1. *[优化] 限定所有的实例资源维护必须用智能指针
    2. *[新增] 调用栈增加头结点，头结点的命令设为@Root
    3. [修复] 修复智能指针和栈上创建混用导致的崩溃问题

--- BindObj.h
+++ BindObj.h
@@ -10,7 +10,7 @@
 #include <cstdio>
 #include "CmdOption.h"
 
-// 绑定函数对象例程
+// 缁戝畾鍑芥暟瀵硅薄渚嬬▼
 struct obj_bind0{
     int m;
     obj_bind0(int _m): m(_m){}
@@ -72,16 +72,16 @@ struct obj_bind3{
 };
 
 void bind_obj_init() {
-    puts("绑定函数对象");
+    puts("缁戝畾鍑芥暟瀵硅薄");
 
-    copt::CmdOption co;
-    co.BindCmd("-obj, --object, --bind_object", obj_bind0(105));
-    co.BindCmd("-obj1, --object1", obj_bind1(205), 206);
-    co.BindCmd("-obj2, --object2", obj_bind2(305), 306, 30.6);
-    co.BindCmd("-obj3, --object3", obj_bind3(405), 406, 40.6, true);
-    co.Start("-obj p1 p2 p3 -obj1 test -obj2 -obj3");
+    copt::CmdOption::ptr_type co = copt::CmdOption::Create();
+    co->BindCmd("-obj, --object, --bind_object", obj_bind0(105));
+    co->BindCmd("-obj1, --object1", obj_bind1(205), 206);
+    co->BindCmd("-obj2, --object2", obj_bind2(305), 306, 30.6);
+    co->BindCmd("-obj3, --object3", obj_bind3(405), 406, 40.6, true);
+    co->Start("-obj p1 p2 p3 -obj1 test -obj2 -obj3");
 
-    puts("绑定函数对象 测试完成\n");
+    puts("缁戝畾鍑芥暟瀵硅薄 娴嬭瘯瀹屾垚\n");
 }
 
 #endif
